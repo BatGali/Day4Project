2673361930afd71fb27bfdcc5a607fa732f10630
Marina <balatcaiam.@gmail.com>
2021-05-25 19:23:51 -0400

    shared on github

--- src/test/java/BranchingPractice.java
+++ src/test/java/BranchingPractice.java
@@ -1,6 +1,13 @@
 public class BranchingPractice {
     public static void main(String[] args) {
-        
+
+        // creating new project
+        //enabled version control
+        //added.gitignore with .idea, target folder and .iml file
+        //made initial commit by keeping track of all files
+        //share project on git hub
+
+
     }
 
 }
